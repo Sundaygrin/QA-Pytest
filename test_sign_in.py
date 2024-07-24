894a48c685cfc23d7a6404bd56527292991b8abd
Sunday charles <sundaycharles61@gmail.com>
2024-07-24 17:42:24 +0100

    first commit

--- test_sign_in.py
+++ test_sign_in.py
@@ -12,7 +12,7 @@ user_data = {
 def test_sign_in():
     response = requests.post(BASE_URL, json=user_data)
 
-    # Print response content for debugging
+
     print("Response Status Code:", response.status_code)
     print("Response Body:", response.text)
 
@@ -21,13 +21,10 @@ def test_sign_in():
         if response_data["message"] == "Redirecting to adjutor app":
             redirect_url = response_data["data"]["redirect_url"]
             print(f"Redirecting to: {redirect_url}")
-            # Handle the redirect if needed, for example by making another request
-            # redirected_response = requests.get(redirect_url)
-            # print("Redirected Response Status Code:", redirected_response.status_code)
-            # print("Redirected Response Body:", redirected_response.text)
+
             return
 
-    # Check if the response status code is 200 (OK)
+
     assert response.status_code == 200, f"Expected status code 200 but got {response.status_code}"
 
 if __name__ == "__main__":
--- test_signup.py
+++ test_signup.py
@@ -1,10 +1,10 @@
 import pytest
 import requests
 
-# Base URL for the API
+
 BASE_URL = "https://vigil.lendsqr.com/pecunia/api/v2/onboard"
 
-# Sample user data for sign up
+
 user_data = {
     "business_name": "the",
     "email": "sundaycharles61@gmail.com",
@@ -19,11 +19,11 @@ user_data = {
 def test_sign_up():
     response = requests.post(BASE_URL, json=user_data)
 
-    # Print response content for debugging
+
     print("Response Status Code:", response.status_code)
     print("Response Body:", response.text)
 
-    # Check if the response status code is 200 (OK)
+
     assert response.status_code == 200, f"Expected status code 200 but got {response.status_code}"
 
 
