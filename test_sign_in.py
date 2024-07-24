4e5c83576573e8621a329eb25a95a76590d3c828
Sunday charles <sundaycharles61@gmail.com>
2024-07-24 18:12:57 +0100

    Updated

--- test_sign_in.py
+++ test_sign_in.py
@@ -4,12 +4,12 @@ import requests
 
 BASE_URL = "https://vigil.lendsqr.com/pecunia/api/v2/auth/admin/login"
 
-user_data = {
-    "email": "sundaycharles61@gmail.com",
-    "password": "Sunday@222"
-}
 
 def test_sign_in():
+    user_data = {
+        "email": "sundaycharles61@gmail.com",
+        "password": "Sunday@222"
+    }
     response = requests.post(BASE_URL, json=user_data)
 
 
@@ -26,6 +26,73 @@ def test_sign_in():
 
 
     assert response.status_code == 200, f"Expected status code 200 but got {response.status_code}"
+
+def test_invalid_email():
+        user_data = {
+            "email": "sundaycharles61gmail.com",
+            "password": "Sunday@222"
+        }
+        response = requests.post(BASE_URL, json=user_data)
+
+        print("Response Status Code:", response.status_code)
+        print("Response Body:", response.text)
+
+        if response.status_code == 400:
+            response_data = response.json()
+            if response_data["message"] == "Redirecting to adjutor app":
+                redirect_url = response_data["data"]["redirect_url"]
+                print(f"Redirecting to: {redirect_url}")
+
+                return
+
+        assert response.status_code == 400, f"Expected status code 400 but got {response.status_code}"
+
+
+def test_invalid_password():
+    user_data = {
+        "email": "sundaycharles61@gmail.com",
+        "password": ""
+    }
+    response = requests.post(BASE_URL, json=user_data)
+
+
+    print("Response Status Code:", response.status_code)
+    print("Response Body:", response.text)
+
+    if response.status_code == 400:
+        response_data = response.json()
+        if response_data["message"] == "Redirecting to adjutor app":
+            redirect_url = response_data["data"]["redirect_url"]
+            print(f"Redirecting to: {redirect_url}")
+
+            return
+
+
+    assert response.status_code == 400, f"Expected status code 400 but got {response.status_code}"
+
+
+
+def test_unauthorized_user():
+    user_data = {
+        "email": "sundaycharles207@gmail.com",
+        "password": "Sunday@222"
+    }
+    response = requests.post(BASE_URL, json=user_data)
+
+
+    print("Response Status Code:", response.status_code)
+    print("Response Body:", response.text)
+
+    if response.status_code == 400:
+        response_data = response.json()
+        if response_data["message"] == "Redirecting to adjutor app":
+            redirect_url = response_data["data"]["redirect_url"]
+            print(f"Redirecting to: {redirect_url}")
+
+            return
+
+
+    assert response.status_code == 400, f"Expected status code 400 but got {response.status_code}"
 
 if __name__ == "__main__":
     pytest.main()
--- test_signup.py
+++ test_signup.py
@@ -4,19 +4,17 @@ import requests
 
 BASE_URL = "https://vigil.lendsqr.com/pecunia/api/v2/onboard"
 
-
-user_data = {
-    "business_name": "the",
-    "email": "sundaycharles61@gmail.com",
-    "locale": "en-US",
-    "name": "John Doe",
-    "password": "SecurePassword123!",
-    "phone_number": "2347033490197",
-    "rc_number": "6752",
-
-}
-
 def test_sign_up():
+    user_data = {
+        "business_name": "the",
+        "email": "sundaycharles61@gmail.com",
+        "locale": "en-US",
+        "name": "John Doe",
+        "password": "SecurePassword123!",
+        "phone_number": "2347033490197",
+        "rc_number": "6752",
+
+    }
     response = requests.post(BASE_URL, json=user_data)
 
 
@@ -25,6 +23,46 @@ def test_sign_up():
 
 
     assert response.status_code == 200, f"Expected status code 200 but got {response.status_code}"
+
+def test_invalid_email():
+    user_data = {
+        "business_name": "the",
+        "email": "sundaycharles61gmail.com",
+        "locale": "en-US",
+        "name": "John Doe",
+        "password": "SecurePassword123!",
+        "phone_number": "2347033490197",
+        "rc_number": "6752",
+
+    }
+    response = requests.post(BASE_URL, json=user_data)
+
+
+    print("Response Status Code:", response.status_code)
+    print("Response Body:", response.text)
+
+
+    assert response.status_code == 400, f"Expected status code 400 but got {response.status_code}"
+
+def test_invalid_password():
+    user_data = {
+        "business_name": "the",
+        "email": "sundaycharles61@gmail.com",
+        "locale": "en-US",
+        "name": "John Doe",
+        "password": "Secu!",
+        "phone_number": "2347033490197",
+        "rc_number": "6752",
+
+    }
+
+    response = requests.post(BASE_URL, json=user_data)
+
+    print("Response Status Code:", response.status_code)
+    print("Response Body:", response.text)
+
+
+    assert response.status_code == 400, f"Expected status code 400 but got {response.status_code}"
 
 
 if __name__ == "__main__":
